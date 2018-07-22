4eced73339c1548c709dc25743744574b6efeb53
Sujay Nair <sujaynair03@gmail.com>
2018-07-22 18:53:40 -0400

    homework 1for thingn MIT

---
+++ hw1/hw1.py
@@ -0,0 +1,43 @@
+# Name:
+# Section: 
+# Date:
+# hw1.py
+
+##### Template for Homework 1, exercises 1.2-1.5 ######
+
+print "********** Exercise 1.2 **********"
+
+# Do your work for Exercise 1.2 here
+
+print "   |  |  \n -------- \n   |  |  \n -------- \n   |  |  " # Delete this line when you write your code!
+
+
+print "********** Exercise 1.3 **********"
+
+a = "   |  |  "
+b = " --------"
+print a +"\n"+ b +"\n" +a +"\n" +b+ "\n"+a
+
+import math
+
+print "********** Exercise 1.4 **********"
+print "********* Part II *************"
+d= (3*5)/(2+3)
+e=math.sqrt((7+9)) * 2
+f= (4-7)**3
+g=(-19+100)**(1.0/4)
+h=6%4
+print (d,e,f,g,h) # Delete this line when you write your code!
+
+print "********* Part III *************"
+j = 2*(5 + 7)**2
+k = 2*5+(7)**2
+print (j,k) # Delete this line when you write your code!
+first_name = raw_input("What is your first name?: ")
+last_name= raw_input("What is your last name?: ")
+month = raw_input("what month were u born?: ")
+year = raw_input("what year were u born?: ")
+day = raw_input("what day were u born?: ")
+print "********** Exercise 1.5 **********"
+
+print ("Your name is " + first_name + " "+ last_name + " and you were born "+ month+" " +day+ " "+ year) # Delete this line when you write your code!
